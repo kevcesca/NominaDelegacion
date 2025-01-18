e16c48656979bce970d7b1e32aa9cb3e3b9f1c52
Garito05 <jampol.diaz@programadoresjava.mx>
2025-01-17 19:44:07 -0600

    Cambios recientes 17/01/25

--- src/app/%Config/apiConfig.js
+++ src/app/%Config/apiConfig.js
@@ -1,9 +1,9 @@
 // src/%config/apiConfig.js
-const API_BASE_URL = 'http://192.168.100.25:7080/Nomina';
+const API_BASE_URL = 'http://192.168.0.68:7080/Nomina';
 
 
 
-export const API_USERS_URL = "http://192.168.0.32:3001";
+export const API_USERS_URL = "http://192.168.0.37:3001";
 
 
 
